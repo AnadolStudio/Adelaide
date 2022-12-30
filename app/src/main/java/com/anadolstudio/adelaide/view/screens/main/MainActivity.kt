cda0c4a4154869ad6af5f5e9809def64781d8505
Anadol <74777850+AnadolStudio@users.noreply.github.com>
2022-12-30 19:29:58 +0300

    cherry-pick from feature/confetti

--- app/src/main/java/com/anadolstudio/adelaide/view/screens/main/MainActivity.kt
+++ app/src/main/java/com/anadolstudio/adelaide/view/screens/main/MainActivity.kt
@@ -3,19 +3,20 @@ package com.anadolstudio.adelaide.view.screens.main
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
 import com.anadolstudio.adelaide.App
 import com.anadolstudio.adelaide.databinding.ActivityMainBinding
+import com.anadolstudio.adelaide.domain.utils.confetti.WinterConfettiGenerator
 import com.anadolstudio.adelaide.view.screens.gallery.GalleryListActivity
+import com.anadolstudio.core.view.animation.AnimateUtil.scaleAnimationOnClick
 
 class MainActivity : AppCompatActivity() {
     companion object {
 
         fun start(context: Context) = context.startActivity(
-                Intent(
-                        context,
-                        MainActivity::class.java
-                )
+            Intent(context, MainActivity::class.java)
         )
     }
 
@@ -26,9 +27,17 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
 
         binding.nightBtn.setOnClickListener { (application as App).changeTheme() }
-        binding.photoCardView.setOnClickListener { startActivity(EditType.PHOTO) }
+        binding.photoCardView.scaleAnimationOnClick { startActivity(EditType.PHOTO) }
 //        binding.collageCardView.setOnClickListener {  } TODO
         setContentView(binding.root)
+
+        Handler(mainLooper).postDelayed(
+            { WinterConfettiGenerator(
+                binding.confettiContainer,
+                AppCompatDelegate.getDefaultNightMode() == AppCompatDelegate.MODE_NIGHT_YES
+                ).launchConfetti()
+            },
+            500)
     }
 
     private fun startActivity(data: EditType) {
