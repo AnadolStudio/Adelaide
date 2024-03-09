ab7fd3afdcc6e4c8d5a9c14089991b767186bf75
Bogdan Terehov <bogdan.terehov>
2024-03-09 23:38:53 +0300

    feature: удалил лишний код

--- app/src/main/java/com/anadolstudio/adelaide/App.kt
+++ app/src/main/java/com/anadolstudio/adelaide/App.kt
@@ -1,33 +1,14 @@
 package com.anadolstudio.adelaide
 
 import android.app.Application
-import android.content.res.Configuration
-import androidx.appcompat.app.AppCompatDelegate.*
-import com.anadolstudio.adelaide.data.SettingsPreference
 import com.anadolstudio.adelaide.di.DI
 
 class App : Application() {
 
     override fun onCreate() {
         super.onCreate()
-        setDefaultNightMode(SettingsPreference.getNightMode(this))
 
         DI.init(applicationContext)
     }
 
-    fun changeTheme() {
-        val mode = when (getDefaultNightMode()) { // TODO это можно вынести за пределы App
-            MODE_NIGHT_FOLLOW_SYSTEM -> getNightModeFromSystem()
-            MODE_NIGHT_NO -> MODE_NIGHT_YES
-            else /*MODE_NIGHT_YES*/ -> MODE_NIGHT_NO
-        }
-
-        SettingsPreference.setNightMode(this, mode)
-        setDefaultNightMode(mode)
-    }
-
-    private fun getNightModeFromSystem(): Int = when (resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK) {
-        Configuration.UI_MODE_NIGHT_NO -> MODE_NIGHT_YES
-        else -> MODE_NIGHT_NO
-    }
 }
--- app/src/main/java/com/anadolstudio/adelaide/base/adapter/BaseGroupAdapter.kt
+++ app/src/main/java/com/anadolstudio/adelaide/base/adapter/BaseGroupAdapter.kt
@@ -4,6 +4,7 @@ import com.xwray.groupie.GroupAdapter
 import com.xwray.groupie.GroupieViewHolder
 import com.xwray.groupie.Section
 
+@Deprecated("Добавлено в core")
 open class BaseGroupAdapter(vararg sections: Section) : GroupAdapter<GroupieViewHolder>() {
 
     init {
